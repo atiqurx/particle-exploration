2df0d01f8abf8c4ff2a6b753917edc92cd61210c
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-03 14:33:11 -0500

    add detection 1. position need to be updaetd

--- environment.py
+++ environment.py
@@ -10,7 +10,14 @@ surface_parameters = {
     3: 100,  # py 100
     4: -100,  # py -100
     5: 37,  # pz 37
-    6: 87  # pz 87
+    6: 87,  # pz 87
+    103: 21.05, # Det 1 outer plane active region
+    104: 8.35, # Det 1 inner plane active region
+    109: 6.35, # Det 1,2,3 back plane of active region
+    113: -6.35, # Det 1,2,3 front plane of active region
+    116: 7.62, # Det 1,2,3 top plane of active region
+    119: -7.62, # Det 1,2,3 bottom plane of active region
+
 }
 soil_dimensions = {
     'x': surface_parameters[1] - surface_parameters[2],
@@ -26,6 +33,16 @@ scene.background = color.black
 soil_volume = box(pos=vector(0, 0, 0),
                    size=vector(soil_dimensions['x'], soil_dimensions['y'], soil_dimensions['z']),
                    color=color.green)
+
+detector_1_dimensions = {
+    'x': surface_parameters[109] - surface_parameters[113],
+    'y': surface_parameters[103] - surface_parameters[104],
+    'z': surface_parameters[116] - surface_parameters[119]
+}
+
+detector_1 =box(pos=vector(100, 0, 40), # need to check if the position is correct
+                   size=vector(detector_1_dimensions['x'], detector_1_dimensions['y'], detector_1_dimensions['z']),
+                   color=color.red)
 
 # Create x, y, and z axes
 x_axis = arrow(pos=vector(0, 0, 0), axis=vector(200, 0, 0), shaftwidth=1, color=color.red)
