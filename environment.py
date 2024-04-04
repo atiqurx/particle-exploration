0ea97d5ee578cf45c53cdde48e8b1df3a5e85988
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-04 02:45:23 -0500

    add pb of shelding

--- environment.py
+++ environment.py
@@ -3,12 +3,14 @@ from vpython import canvas, box, vector, color, rate, arrow
 # Define the extracted values from the input file (seed_0.inp)
 
 surface_parameters = {
+    # surface volume
     1: 100,  # px 100
     2: -100,  # px -100
     3: 100,  # py 100
     4: -100,  # py -100
     5: 37,  # pz 37
     6: 87,  # pz 87
+    # detectors
     103: 21.05, # Det 1 outer plane active region
     104: 8.35, # Det 1 inner plane active region
     105: 6.35, # Det 2 plane on Det 1 side active region
@@ -19,6 +21,13 @@ surface_parameters = {
     113: -6.35, # Det 1,2,3 front plane of active region
     116: 7.62, # Det 1,2,3 top plane of active region
     119: -7.62, # Det 1,2,3 bottom plane of active region
+    # sheilding
+    65: 5, # bottom surface of shielding. from MINS field scale scan, bottom is 5cm below neutron target
+    66: -15, # top surface of shielding 
+    611: 40, # Detector side surface of Pb   
+    612: 35, # Generator side surface of Pb     
+    613: 30.5, # Side surface of Pb      
+    614: -30.5, # Side surface of Pb 
 
 }
 soil_dimensions = {
@@ -34,7 +43,7 @@ scene.background = color.black
 # Create a box to represent the soil volume
 soil_volume = box(pos=vector(0, 0, 0),
                    size=vector(soil_dimensions['x'], soil_dimensions['y'], soil_dimensions['z']),
-                   color=color.green)
+                   color=color.blue)
 
 
 # DETECTORS
@@ -46,7 +55,7 @@ detector_1_dimensions = {
 
 detector_1 = box(pos=vector(65 ,  20, 60), # need to check if the position is correct
                    size=vector(detector_1_dimensions['x'], detector_1_dimensions['y'], detector_1_dimensions['z']),
-                   color=color.red)
+                   color=color.cyan)
 
 detector_2_dimensions = {
     'x': surface_parameters[109] - surface_parameters[113],
@@ -56,7 +65,7 @@ detector_2_dimensions = {
 
 detector_2 = box(pos=vector(65 , 0 , 60), # need to check if the position is correct
                    size=vector(detector_2_dimensions['x'], detector_2_dimensions['y'], detector_2_dimensions['z']),
-                   color=color.red)
+                   color=color.cyan)
 
 detector_3_dimensions = {
     'x': surface_parameters[109] - surface_parameters[113],
@@ -66,7 +75,18 @@ detector_3_dimensions = {
 
 detector_3 = box(pos=vector(65 , -20 , 60), # need to check if the position is correct
                    size=vector(detector_3_dimensions['x'], detector_3_dimensions['y'], detector_3_dimensions['z']),
-                   color=color.red)
+                   color=color.cyan)
+
+# SHIELDING
+pb_dimensions = {
+    'x': surface_parameters[611] - surface_parameters[612],
+    'y': surface_parameters[613] - surface_parameters[614],
+    'z': surface_parameters[65] - surface_parameters[66]
+}
+
+pb = box(pos=vector(40 , 0 , 60), # need to check if the position is correct
+                   size=vector(pb_dimensions['x'], pb_dimensions['y'], pb_dimensions['z']),
+                   color=color.gray(0.5))
 
 # Create x, y, and z axes
 x_axis = arrow(pos=vector(0, 0, 0), axis=vector(500, 0, 0), shaftwidth=1, color=color.red)
